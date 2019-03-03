80424d240dbb8ca04579041822eeec75dc51ecd2
ahya37 <ahmadyaniid37@gmail.com>
2019-03-03 21:44:47 +0700

    penambahan komen 2

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!-- komentar -->
+<!-- lagi belajar -->
 <?php include 'function/title.php'; ?>
 <center><h1>SELAMAT DATANG</h1> <center><h1>SISTEM PAKAR DIAGNOSIS PENYAKIT MATA KATARAK </h1>
 <br/>
